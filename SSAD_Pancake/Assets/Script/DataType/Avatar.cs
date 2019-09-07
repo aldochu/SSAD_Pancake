3d23a64552896699a84f62980bebbbeeb984779e
aldochu <aldochu@gmail.com>
2019-09-07 22:47:41 +0800

    Added script to manipulate database

---
+++ SSAD_Pancake/Assets/Script/DataType/Avatar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour
+{
+    public string head;
+    public string face;
+    public string shirt;
+    public string pant;
+
+    public Avatar()
+    {
+    }
+
+    public Avatar(string head, string face, string shirt, string pant)
+    {
+        this.head = head;
+        this.face = face;
+        this.shirt = shirt;
+        this.pant = pant;
+    }
+}
---
+++ SSAD_Pancake/Assets/Script/DataType/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+
+    public string userid;
+    public string email;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email)
+    {
+        this.userid = username;
+        this.email = email;
+
+    }
+}
